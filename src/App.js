9069293df468c280435a3bcd1234241598d47e2a
OffJosh <jlmerrett@gmail.com>
2024-04-26 22:27:33 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+let discordLogo = "./discord-mark-white.svg";
+let instagramLogo = "./Instagram_Glyph_White.svg";
+let youtubeLogo = "https://assets-global.website-files.com/6257adef93867e50d84d30e2/65a4fe42d907d27f3dead7ad_youtube.svg";
+let githubLogo = "./github-mark-white.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Content/>
     </div>
   );
 }
+
+function Header(){
+  return(
+    <div className="headerDiv">
+      <img className="headerPhoto" src="./kermit512.png"/>
+    </div>
+  )
+}
+
+function Content(){
+  return(
+    <div>
+      <Tile params={[discordLogo, "Discord: offjosh", "https://discord.com/channels/@me"]}/>
+      <Tile params={[githubLogo, "GitHub", "https://github.com/OffJosh"]}/>
+      <Tile params={[instagramLogo, "Instagram", "https://www.instagram.com/merrettjosh_/"]}/>
+    </div>
+  )
+}
+
+function Tile({params}, alt) {
+  return (
+    <a className="text" href={params[2]} target="_blank">
+      <div className="outerTile">
+        <div className="tile">
+          <div className="left">
+            <img className="logo" src={params[0]}></img>
+          </div>
+          <div className="right">
+            <a className="text" >{params[1]}</a><br/>
+          </div>      
+        </div>
+      </div>
+    </a>
+  );
+}
+
 
 export default App;
