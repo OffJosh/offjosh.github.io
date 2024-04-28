f96e2d0fa3cebc63f6b56ebc4a4d100d6b7ae3e7
OffJosh <jlmerrett@gmail.com>
2024-04-28 18:02:51 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@
 import './App.css';
 
 let discordLogo = "./discord-mark-white.svg";
+let discordLink = "https://discord.com/channels/@me";
+
 let instagramLogo = "./Instagram_Glyph_White.svg";
+let instagramLink = "https://www.instagram.com/merrettjosh_/";
+
 let youtubeLogo = "https://assets-global.website-files.com/6257adef93867e50d84d30e2/65a4fe42d907d27f3dead7ad_youtube.svg";
+
 let githubLogo = "./github-mark-white.svg";
+let githubLink = "https://github.com/OffJosh";
+
+let wallpapersV1Link = "https://drive.google.com/drive/folders/1AzI0Lv2Jju-tyaYuTjB503ocl27iXKzp?usp=sharing"
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Content/>
+      <Footer />
     </div>
   );
 }
@@ -23,17 +32,38 @@ function Header(){
   )
 }
 
-function Content(){
+function Footer(){
   return(
-    <div>
-      <Tile params={[discordLogo, "Discord: offjosh", "https://discord.com/channels/@me"]}/>
-      <Tile params={[githubLogo, "GitHub", "https://github.com/OffJosh"]}/>
-      <Tile params={[instagramLogo, "Instagram", "https://www.instagram.com/merrettjosh_/"]}/>
+    <div className="footerDiv">
     </div>
   )
 }
 
-function Tile({params}, alt) {
+function Content(){
+  return(
+    <div>
+      <Tile params={[discordLogo, "Discord: offjosh", discordLink]}/>
+      <Tile params={[githubLogo, "GitHub", githubLink]}/>
+      <Tile params={[instagramLogo, "Instagram", instagramLink]}/>
+      <Tile params={[null, "Wallpapers", wallpapersV1Link]}/>
+    </div>
+  )
+}
+
+function Tile({params}, alt) {  
+  if (params[0] == null){
+    return(
+    <a className="text" href={params[2]} target="_blank">
+      <div className="outerTile">
+        <div className="tile">
+          <div className="center">
+            <a className="text" >{params[1]}</a><br/>
+          </div>      
+        </div>
+      </div>
+    </a>
+    )
+  }
   return (
     <a className="text" href={params[2]} target="_blank">
       <div className="outerTile">
