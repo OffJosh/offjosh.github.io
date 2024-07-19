7c022612416832eef5e168bca36c56ba3dba4273
OffJosh <jlmerrett@gmail.com>
2024-07-19 23:50:09 +0100

    make home page the links page

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,18 @@ import {SiteTile} from './Tiles'
 import {Tile} from "./Tiles";
 import { useEffect } from "react";
 
-let wallpapersLogo = "./hamburger2.png"
+let discordLogo = "./discord-mark-white.svg";
+let discordLink = "https://discord.com/channels/@me";
+
+let instagramLogo = "./Instagram_Glyph_White.svg";
+let instagramLink = "https://www.instagram.com/merrettjosh_/";
+
+let youtubeLogo = "https://assets-global.website-files.com/6257adef93867e50d84d30e2/65a4fe42d907d27f3dead7ad_youtube.svg";
+
+let githubLogo = "./github-mark-white.svg";
+let githubLink = "https://github.com/OffJosh";
+
+let wallpapersLogo = "./desktop-512.png"
 
 
 
@@ -15,7 +26,10 @@ export function Home(props){
     })
     return(
         <div>
-        <SiteTile params={[wallpapersLogo, "Links", "/links"]}/>
+        <Tile params={[discordLogo, "Discord: offjosh", discordLink]}/>
+        <Tile params={[githubLogo, "GitHub", githubLink]}/>
+        <Tile params={[instagramLogo, "Instagram", instagramLink]}/>
+        <SiteTile params={[wallpapersLogo, "Wallpapers", "/wallpapers"]}/>
         </div>
     )
 }
